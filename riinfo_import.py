299c899da4ce680acd0459951b94f16c531a50a4
steinhb <b.steinh@gmail.com>
2022-03-28 11:04:01 +0200

    commit

--- riinfo_import.py
+++ riinfo_import.py
@@ -1,6 +1,24 @@
 from io import StringIO
 import numpy as np
-import loadyaml_url as lyurl
+import urllib.request  # the lib that handles the url stuff
+import yaml
+
+# Load a yaml file from url
+def loadyaml_url(url):
+    """loadyaml_url(url)
+
+    Args:
+        url (string):url to the yaml file
+
+    Returns:
+        data: yaml object with the opened data
+    """
+    # Open and read the url -> decode bytestream to utf8
+    db_lib = urllib.request.urlopen(url).read().decode('utf8')
+    # Load yaml encoded file
+    data = yaml.safe_load(db_lib)
+    return data
+
 
 class riinfo:
     def __init__(self, **kwargs):
@@ -9,7 +27,7 @@ class riinfo:
     def load_complete_db_index(self):
         print('Loading database index')
         default_url = "https://raw.githubusercontent.com/polyanskiy/refractiveindex.info-database/master/database/library.yml"
-        self.db_lib=lyurl.loadyaml_url(default_url)
+        self.db_lib=loadyaml_url(default_url)
         self.index_loaded = True
         
     def search_for_material(self, mat):
@@ -45,7 +63,7 @@ class riinfo:
     def load_material(self,path_to_material):
         loadurl="https://raw.githubusercontent.com/polyanskiy/refractiveindex.info-database/master/database/data/"
         loadurl+=path_to_material        
-        data=lyurl.loadyaml_url(loadurl)
+        data=loadyaml_url(loadurl)
         
         return np.genfromtxt(StringIO(data['DATA'][0]['data']))
     
